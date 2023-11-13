1c907abc64386f48d0a7df2aebf4eae18910ab96
Pranav10Chauhan <110055742+Pranav10Chauhan@users.noreply.github.com>
2023-11-13 19:16:28 +0000

    config container

---
+++ basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strict" //treat code as new javascript standards
+//alery(3+3) alert does not work here as thus is nodejs
+//code readabiltity should be high
+
+let name="Pranav"//String
+let age=23 //numbers
+let isloggenin=true// boolean
+//bigint
+//null-standalone value
+let temp=null;
+//symbol-uniqueness
+//undefined
+//object
+console.log(typeof name)
+console.log(name)
--- variables/practice.js
+++ variables/practice.js
@@ -1,6 +1,7 @@
 console.log("practice")
-let name="pranav"
+let name="pranav"//use mostly this
 const num=1234;
-var sirname='Chauhan';
+var sirname='Chauhan';//old way
+
 state="hp";
 console.table([num,sirname,state])
