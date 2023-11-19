95ab14a9289557d4dbe8a7ed4526700621001bce
Pranav10Chauhan <110055742+Pranav10Chauhan@users.noreply.github.com>
2023-11-19 17:51:45 +0000

    comparisions

--- basics/ConversionsOperations.js
+++ basics/ConversionsOperations.js
@@ -13,3 +13,17 @@ console.log(boolloggen)
 //for boolean 0 is false 1is true
 //empty strings is false and filled strings is true
 //can be converted into strings too
+
+
+//Operations
+//normal arithmetic operations applies like addition,subtractionsetc
+let name='Pranav'
+let sirname='Chauhan'
+console.log(name+sirname)
+console.log(1+"2")
+console.log("1"+2)
+console.log(1+"2"+"2")
+console.log(1+2+"2")
+
+let num1,num2,num3
+num1=num2=num3=2+2
---
+++ basics/comparisions.js
@@ -0,0 +1,10 @@
+console.log(2>1)
+//comparsions same as elsewhere 
+//always compare in similar datatypes as javascript allows it can be unpredictable sometimes
+console.log("2">1)
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+// === stritct check value and datatypes
+console.log(2==="2")
