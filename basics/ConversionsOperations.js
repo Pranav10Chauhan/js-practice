0e1b190af585315909c930596c38d23ecb122ac9
Pranav10Chauhan <110055742+Pranav10Chauhan@users.noreply.github.com>
2023-11-19 17:04:54 +0000

    conversions of datatypes

---
+++ basics/ConversionsOperations.js
@@ -0,0 +1,15 @@
+let score="33"
+console.log(typeof score)
+
+let value=Number(score)
+console.log(typeof value)
+let score1="33abc"
+let value2=Number(score1)
+//null into 0
+//true/false into 1/0
+let loggedin=1
+let boolloggen=Boolean(loggedin)
+console.log(boolloggen)
+//for boolean 0 is false 1is true
+//empty strings is false and filled strings is true
+//can be converted into strings too
